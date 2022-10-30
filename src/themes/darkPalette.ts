d5bdeee509beaa87a7535b5ec9c8c32a69fc07ee
Lachlan Malec <lachlan@lachlanmalec.dev>
2022-10-30 16:17:25 +1000

    feat: created custom mui themes

---
+++ src/themes/darkPalette.ts
@@ -0,0 +1,47 @@
+import { PaletteOptions } from '@mui/material/styles';
+
+const darkPalette: PaletteOptions = {
+  mode: 'dark',
+  primary: {
+    main: '#458588',
+    light: '#83a598',
+    contrastText: '#fbf1c7',
+  },
+  secondary: {
+    main: '#b16286',
+    light: '#d3869b',
+    contrastText: '#fbf1c7',
+  },
+  background: {
+    default: '#282828',
+    paper: '#282828',
+  },
+  text: {
+    primary: '#fbf1c7',
+    secondary: '#ebdbb2',
+    disabled: '#d5c4a1',
+  },
+  error: {
+    main: '#cc241d',
+    light: '#fb4934',
+    contrastText: '#fbf1c7',
+  },
+  warning: {
+    main: '#d79921',
+    light: '#fabd2f',
+    contrastText: '#fbf1c7',
+  },
+  info: {
+    main: '#458588',
+    light: '#83a598',
+    contrastText: '#fbf1c7',
+  },
+  success: {
+    main: '#689d6a',
+    light: '#8ec07c',
+    contrastText: '#fbf1c7',
+  },
+  divider: '#32302f',
+};
+
+export default darkPalette;
