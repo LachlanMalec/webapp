8e32d0e8b4bf85948e59094e0f875a90677f1299
Lachlan Malec <lachlan@lachlanmalec.dev>
2022-11-10 23:03:10 +1000

    refactor: removed MUI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-//MUI Components
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-//Custom Themes
-import lightPalette from './themes/lightPalette';
-import darkPalette from './themes/darkPalette';
-//Custom Components
-import Landing from './pages/Landing';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
-interface EvelvateOnScrollProps {
-  children: React.ReactElement;
-}
-const ElevateOnScroll: React.FunctionComponent<EvelvateOnScrollProps> = (props: EvelvateOnScrollProps) => {
-  const { children } = props;
-  const trigger = useScrollTrigger({
-    disableHysteresis: false,
-    threshold: 0,
-  });
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
-};
-const App: React.FunctionComponent<Record<string, never>> = () => {
-  const [mode, setMode] = React.useState<'light' | 'dark'>('dark');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    []
-  );
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: mode === 'light' ? lightPalette : darkPalette,
-      }),
-    [mode]
-  );
+const App: React.FunctionComponent = () => {
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <ElevateOnScroll>
-          <AppBar>
-            <Toolbar>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Lachlan Malec
-              </Typography>
-              <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
-                {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-        </ElevateOnScroll>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Toolbar />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-          </Routes>
-        </Box>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="about" element={<h1>About</h1>} />
+    </Routes>
   );
 };
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-//MUI Font
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
