31b033fabde96879e794876f64fe8799222c6d55
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-05 21:59:54 +1000

    fix: dark mode hover in mobile menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -97,7 +97,7 @@ export default function Navbar({ navigation }: Props) {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 dark:text-gray-100 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 dark:text-gray-100 text-gray-900 dark:hover:bg-slate-900 hover:bg-gray-50"
                   >
                     {item.name}
                   </a>
