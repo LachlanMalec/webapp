4862f768adc8a97a2a62252cc60e8eb5e0060b07
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-05 20:38:45 +1000

    refactor: move common layout to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center">
-        <h1>Lachlan Malec</h1>
+        <h1 className=''>Lachlan Malec</h1>
       </div>
-    </main>
   )
 }
