bfa720a7bdde504d287c319374b05148f78d6e44
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-06 19:09:09 +1000

    feat: gruvbox dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 dark:text-slate-400 ring-1 ring-gray-900/10 dark:ring-slate-400/20 hover:ring-gray-900/20 dark:ring-slate-400/10">
+          <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-fg ring-1 ring-blueDim/80 hover:ring-blueDim/90">
             My latest blog title.{" "}
             <a href="#" className="font-semibold text-indigo-600">
               <span className="absolute inset-0" aria-hidden="true" />
@@ -14,23 +14,23 @@ export default function Home() {
           </div>
         </div>
         <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight dark:text-gray-100 text-gray-900 sm:text-6xl">
+          <h1 className="text-4xl font-bold tracking-tight text-fg sm:text-6xl">
             Lachlan Malec
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-slate-400">
+          <p className="mt-6 text-lg leading-8 text-fg0">
             Hi, I&apos;m Lachlan. Currently I&apos;m studying a Bachelor of Information
             Technology, majoring in Computer Science, at QUT.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="#"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-blueDim px-3.5 py-2.5 text-sm font-semibold text-fg shadow-sm hover:bg-blueDim/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Contact Me
             </a>
             <a
               href="#"
-              className="text-sm font-semibold leading-6 text-gray-900 dark:text-slate-400"
+              className="text-sm font-semibold leading-6 text-fg"
             >
               Find out more <span aria-hidden="true">→</span>
             </a>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -28,17 +28,19 @@ export default function Navbar({ navigation }: Props) {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Lachlan Malec</span>
-            <img
+            <Image
               className="h-8 w-auto"
-              src="https://lachlanmalec.dev/logo.png"
-              alt=""
+              src="/logo.png"
+              height={538}
+              width={538}
+              alt="logo"
             />
           </a>
         </div>
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-slate-400"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-fg"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -50,7 +52,7 @@ export default function Navbar({ navigation }: Props) {
             <a
               key={item.name}
               href={item.href}
-              className="text-sm font-semibold leading-6 text-gray-900 dark:text-slate-400"
+              className="text-sm font-semibold leading-6 text-fg"
             >
               {item.name}
             </a>
@@ -71,19 +73,21 @@ export default function Navbar({ navigation }: Props) {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto dark:bg-slate-800 bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-bg_h px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-fg/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
+              <span className="sr-only">Lachlan Malec</span>
+              <Image
                 className="h-8 w-auto"
-                src="https://lachlanmalec.dev/logo.png"
-                alt=""
+                src="/logo.png"
+                height={538}
+                width={538}
+                alt="logo"
               />
             </a>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5 text-fg"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
@@ -91,13 +95,13 @@ export default function Navbar({ navigation }: Props) {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="-my-6 divide-y divide-text-fg/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 dark:text-gray-100 text-gray-900 dark:hover:bg-slate-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-fg hover:bg-bg"
                   >
                     {item.name}
                   </a>
