7affb92d8eda7d1a2dbf797319933bcb6db149fd
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-10 20:48:31 +1000

    feat: better about me readability

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ export default function About() {
       <h2 className="text-4xl font-bold tracking-tight text-fg sm:text-6xl">
         About Me
       </h2>
-      <p className="mt-6 text-lg text-justify leading-8 text-fg0">
+      <p className="mt-6 text-base px-10 md:px-0 text-justify leading-8 text-fg0">
         Hello! I&apos;m Lachlan Malec, a passionate Full-Stack Software Engineer at
         TestLab360 and a full-time Computer Science student at the Queensland
         University of Technology (QUT). In my professional life, I use my
@@ -14,7 +14,7 @@ export default function About() {
         always eager to learn and explore new tools that can enhance my
         capabilities.
       </p>
-      <p className="mt-6 text-lg text-justify leading-8 text-fg0">
+      <p className="mt-6 text-base px-10 md:px-0 text-justify leading-8 text-fg0">
         When I&apos;m not juggling between coding assignments and professional
         responsibilities, you will likely find me immersed in the exciting world
         of gaming. My love for video games extends beyond just playing them; I&apos;m
@@ -23,14 +23,14 @@ export default function About() {
         one of my long-term goals is to eventually work on software development
         for gaming systems.
       </p>
-      <p className="mt-6 text-lg text-justify leading-8 text-fg0">
+      <p className="mt-6 text-base px-10 md:px-0 text-justify leading-8 text-fg0">
         I also have a fascination for the intriguing world of internet
         mysteries. The endless virtual maze often takes me down rabbit holes
         filled with interesting unsolved mysteries, coding puzzles, and hidden
         easter eggs - each one a thrilling challenge that tickles my curiosity
         and tests my problem-solving abilities.
       </p>
-      <p className="mt-6 text-lg text-justify leading-8 text-fg0">
+      <p className="mt-6 text-base px-10 md:px-0 text-justify leading-8 text-fg0">
         Balancing work, study, and personal interests is not always easy, but I
         love the diversity and the challenge. It keeps me on my toes and
         inspires me to continuously improve. Feel free to connect with me if you
