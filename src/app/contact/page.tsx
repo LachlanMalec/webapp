62dc6543ebd9e46e4b0a4c1572cf05ac16935d30
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-10 19:54:41 +1000

    feat: inclusive name input

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -30,38 +30,22 @@ export default function Contact() {
       </div>
       <form onSubmit={handleSubmit} className="mx-auto mt-10 max-w-xl l:mt-16">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          <div>
+          <div className="sm:col-span-2">
             <label
-              htmlFor="first-name"
+              htmlFor="name"
               className="block text-sm font-semibold leading-6 text-fg"
             >
-              First name
+              Name
             </label>
             <div className="mt-2.5">
               <input
                 type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
+                name="name"
+                id="name"
+                autoComplete="name"
                 className="block w-full bg-bg_h/70 rounded-md border-0 px-3.5 py-2 text-fg shadow-sm ring-1 ring-inset ring-blue hover:ring-blueDim placeholder:text-fg focus:ring-2 focus:ring-inset focus:ring-blue hover:ring-blueDimDim sm:text-sm sm:leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue"
               />
-            </div>
-          </div>
-          <div>
-            <label
-              htmlFor="last-name"
-              className="block text-sm font-semibold leading-6 text-fg"
-            >
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
-                className="block w-full bg-bg_h/70 rounded-md border-0 px-3.5 py-2 text-fg shadow-sm ring-1 ring-inset ring-blue hover:ring-blueDim placeholder:text-fg focus:ring-2 focus:ring-inset focus:ring-blue hover:ring-blueDimDim sm:text-sm sm:leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue"
-              />
+              <ValidationError prefix="Name" field="name" errors={formState.errors} />
             </div>
           </div>
           <div className="sm:col-span-2">
