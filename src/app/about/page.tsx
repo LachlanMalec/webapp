2e2f0a5edf3523643593014cacdf27f6b462eeec
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-10 20:16:36 +1000

    feat: basic about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,42 @@
+export default function About() {
+  return (
+    <div className="mx-auto max-w-2xl xl:pt-24 pt-16 text-center">
+      <h2 className="text-3xl font-bold tracking-tight text-fg0 sm:text-4xl">
+        About Me
+      </h2>
+      <p className="mt-4 text-base leading-8 text-fg">
+        Hello! I&apos;m Lachlan Malec, a passionate Full-Stack Software Engineer at
+        TestLab360 and a full-time Computer Science student at the Queensland
+        University of Technology (QUT). In my professional life, I use my
+        creative problem-solving skills and meticulous attention to detail to
+        ensure seamless functioning of the software I contribute to. I have a
+        keen interest in various technologies and programming languages, and I&apos;m
+        always eager to learn and explore new tools that can enhance my
+        capabilities.
+      </p>
+      <p className="mt-4 text-base leading-8 text-fg">
+        When I&apos;m not juggling between coding assignments and professional
+        responsibilities, you will likely find me immersed in the exciting world
+        of gaming. My love for video games extends beyond just playing them; I&apos;m
+        intrigued by their development process and the programming behind them,
+        which perfectly aligns with my career in software engineering. In fact,
+        one of my long-term goals is to eventually work on software development
+        for gaming systems.
+      </p>
+      <p className="mt-4 text-base leading-8 text-fg">
+        I also have a fascination for the intriguing world of internet
+        mysteries. The endless virtual maze often takes me down rabbit holes
+        filled with interesting unsolved mysteries, coding puzzles, and hidden
+        easter eggs - each one a thrilling challenge that tickles my curiosity
+        and tests my problem-solving abilities.
+      </p>
+      <p className="mt-4 text-base leading-8 text-fg">
+        Balancing work, study, and personal interests is not always easy, but I
+        love the diversity and the challenge. It keeps me on my toes and
+        inspires me to continuously improve. Feel free to connect with me if you
+        share similar interests or have any opportunities you think would pique
+        my interest. Let&apos;s uncover the mysteries of the tech world together!
+      </p>
+    </div>
+  );
+}
