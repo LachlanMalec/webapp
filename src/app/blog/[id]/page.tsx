ffe2a3a1db950b57e1ecadc2296d5130effc3c30
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-13 22:02:27 +1000

    feat: basic blog post view

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+import Error from "next/error";
+
+export default async function Post({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const postId = parseInt(id);
+
+  async function getPost() {
+    "use server";
+    const prisma = new PrismaClient();
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        category: true,
+      },
+    });
+    return post;
+  }
+  const post = await getPost();
+
+  return (
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-2xl lg:mx-0 xl:pt-24 pt-16">
+        <h2 className="text-3xl font-bold tracking-tight text-fg0 sm:text-4xl">
+          {post?.title}
+        </h2>
+        <p className="mt-2 text-lg leading-8 text-fg">
+          {post?.datetime.toLocaleDateString("en-AU", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}{" "}
+          &middot; {post?.category.name}
+        </p>
+      </div>
+      <div className="mx-auto mt-2 max-w-2xl border-t border-fg pt-10 sm:mt-8 sm:pt-16 lg:mx-0 lg:max-w-none">
+        <div className="prose prose-lg text-fg">
+          {post?.description}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,10 +90,10 @@ export default async function Home() {
                   </div>
                   <div className="group relative">
                     <h3 className="mt-3 text-lg font-semibold leading-6 text-fg0 group-hover:text-gray-600">
-                      <a href={post.href}>
+                      <Link href={`/blog/${post.id}`}>
                         <span className="absolute inset-0" />
                         {post.title}
-                      </a>
+                      </Link>
                     </h3>
                     <p className="mt-5 line-clamp-3 text-sm leading-6 text-fg">
                       {post.description}
