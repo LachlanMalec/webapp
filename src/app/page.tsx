1ba9126e3fd02199538b69535af95ebcd74ffa4f
Lachlan Malec <lachlan@lachlanmalec.dev>
2023-06-13 21:07:14 +1000

    refactor: conditional render of blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,53 +57,54 @@ export default async function Home() {
           </div>
         </div>
       </div>
-
-      <div className="pt-24 pb-12 sm:pt-32 sm:pb-16">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-3xl font-bold tracking-tight text-fg0 sm:text-4xl">
-              from the blog
-            </h2>
-            <p className="mt-2 text-lg leading-8 text-fg">
-              some of my thoughts and experiences.
-            </p>
-          </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-fg pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {posts.map((post) => (
-              <article
-                key={post.id}
-                className="flex max-w-xl flex-col items-start justify-between"
-              >
-                <div className="flex items-center gap-x-4 text-xs">
-                  <time
-                    dateTime={post.datetime.toLocaleDateString()}
-                    className="text-fg3"
-                  >
-                    {post.datetime.toLocaleDateString()}
-                  </time>
-                  <a
-                    href={post.category.href}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-fg3 hover:bg-gray-100"
-                  >
-                    {post.category.name}
-                  </a>
-                </div>
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-fg0 group-hover:text-gray-600">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
-                      {post.title}
+      {posts.length > 0 ? (
+        <div className="pt-24 pb-12 sm:pt-32 sm:pb-16">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:mx-0">
+              <h2 className="text-3xl font-bold tracking-tight text-fg0 sm:text-4xl">
+                from the blog
+              </h2>
+              <p className="mt-2 text-lg leading-8 text-fg">
+                some of my thoughts and experiences.
+              </p>
+            </div>
+            <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-fg pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+              {posts.map((post) => (
+                <article
+                  key={post.id}
+                  className="flex max-w-xl flex-col items-start justify-between"
+                >
+                  <div className="flex items-center gap-x-4 text-xs">
+                    <time
+                      dateTime={post.datetime.toLocaleDateString()}
+                      className="text-fg3"
+                    >
+                      {post.datetime.toLocaleDateString()}
+                    </time>
+                    <a
+                      href={post.category.href}
+                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-fg3 hover:bg-gray-100"
+                    >
+                      {post.category.name}
                     </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-fg">
-                    {post.description}
-                  </p>
-                </div>
-              </article>
-            ))}
+                  </div>
+                  <div className="group relative">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-fg0 group-hover:text-gray-600">
+                      <a href={post.href}>
+                        <span className="absolute inset-0" />
+                        {post.title}
+                      </a>
+                    </h3>
+                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-fg">
+                      {post.description}
+                    </p>
+                  </div>
+                </article>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      ) : null}
     </>
   );
 }
